6b7c44326893c79ea28e7da0958473570e286247
Ilia Pushin <avers2000@yandex.ru>
2021-12-19 16:16:11 +0400

    Birthday info screen.

--- app/src/main/java/ru/ilya/pushin/birthdaysappchallenge/features/birthdays/presentation/ui/BirthdayInfoComposable.kt
+++ app/src/main/java/ru/ilya/pushin/birthdaysappchallenge/features/birthdays/presentation/ui/BirthdayInfoComposable.kt
@@ -2,11 +2,17 @@ package ru.ilya.pushin.birthdaysappchallenge.features.birthdays.presentation.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import ru.ilya.pushin.birthdaysappchallenge.R
 import ru.ilya.pushin.birthdaysappchallenge.features.birthdays.domain.models.User
 
 @Composable
@@ -14,10 +20,24 @@ fun BirthdayInfoComposable(
     user: User,
     navigateBack: () -> Unit,
 ) {
-    Column(modifier = Modifier.padding(8.dp)) {
+    Column(
+        modifier = Modifier.padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
-            style = MaterialTheme.typography.subtitle2,
+            modifier = Modifier.size(96.dp),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.h2,
+            text = user.shortName
+        )
+        Text(
+            style = MaterialTheme.typography.h4,
             text = user.name
         )
+        Text(stringResource(id = R.string.age, user.age))
+
+        Button(onClick = navigateBack) {
+            Text(stringResource(id = R.string.go_back))
+        }
     }
 }
